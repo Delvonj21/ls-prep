045f12fd649a205ea1429b565d388d5bf8490b3c
Delvon <delvonj21@gmail.com>
2024-06-10 08:41:33 +0200

    added files

---
+++ intro_collections/exercises.py
@@ -0,0 +1,47 @@
+#1 If you have a list named people, how can you determine the number of entries in that list?
+# len(people)
+
+#2 Can you write some code to change the value 'bye' in the following tuple to 'goodbye'?
+# stuff = ('hello', 'world', 'bye', 'now')
+
+# new_tup = ('hello', 'world', 'goodbye', 'now')
+
+#3 Assuming you have the following assignment:
+# pi = 3.141592
+# Write some code that converts the value of pi to a string and assigns it to a variable named str_pi.
+
+# str_pi = str(pi)
+
+#4 Without running the following code, identify the numbers that are included in each of the following ranges:
+# range(7)            [0, 1, 2, 3, 4, 5, 6]         
+# range(1, 6)         [1, 2, 3, 4, 5]
+# range(3, 15, 4)     [3, 7, 11]
+# range(3, 8, -1)     []
+# range(8, 3, -1)     [8, 7, 6, 5, 4]
+
+#5 How would you print all the numbers in the following range?
+
+# range(3, 17, 4)
+
+# print(list(range(3, 17, 4)))
+
+#6 Consider the data in the following table:
+
+# Name	Country
+# Alice	USA
+# Francois	Canada
+# Inti	Peru
+# Monika	Germany
+# Sanyu	Uganda
+# Yoshitaka	Japan
+
+# countries = {
+#     'Alice' :    'USA',
+#     'Francois':  'Canada',
+#     'Inti':      'Peru',
+#     'Monika':    'Germany',
+#     'Sanyu':     'Uganda',
+#     'Yoshitaka': 'Japan'
+# }
+
+# print('countries'['Alica'])
